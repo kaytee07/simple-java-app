416e02844b5b15750fb2e6356f1bfe82de185ff8
kaytee07 <kofi.taylor07@gmail.com>
2025-09-01 09:39:20 +0000

    my first beanstalk app

---
+++ src/main/java/com/taylor/beanstalkproject/BeanstalkProjectApplication.java
@@ -0,0 +1,21 @@
+package com.taylor.beanstalkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BeanstalkProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanstalkProjectApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+
+}
