175cd13daca9efc466f80e90a6ae2779802c03ea
kaytee07 <kofi.taylor07@gmail.com>
2025-09-04 08:14:10 +0000

    add something different

--- src/main/java/com/taylor/beanstalkproject/BeanstalkProjectApplication.java
+++ src/main/java/com/taylor/beanstalkproject/BeanstalkProjectApplication.java
@@ -15,7 +15,7 @@ public class BeanstalkProjectApplication {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World! Kofi Taylor Winful";
+        return "Hello World! Kofi Taylor Winful Benedict";
     }
 
 }
